33c59998377c63807156d52f1ba073a7f17bc977
Apexofacircle <djadbeatmaker@gmail.com>
2023-06-05 19:09:21 -0400

    Add slideshow feature

---
+++ script.js
@@ -0,0 +1,14 @@
+var _SlideNumber = 0;
+var _MaxSlides = 11;
+function TestKeyPress(){
+    console.log("KeyPressed");
+    _SlideNumber += 1;
+    if (_SlideNumber > _MaxSlides){
+        window.location.href = "#0"
+        _SlideNumber = 0
+    }
+    else{
+        window.location.href = "#" + _SlideNumber.toString();
+    }
+    
+}
